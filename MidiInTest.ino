c0b80388b178287a715f9feaf95e2c3ddfee6689
Eric Jay Rangell <erangell@users.noreply.github.com>
2022-05-11 18:20:31 -0400

    Fixed program change bug & added filtering by chnl

--- MidiInTest.ino
+++ MidiInTest.ino
@@ -3,16 +3,21 @@
 //Requires curcuitry for MIDI IN feeding in to Serial1 Rx1
 //MIDI OUT is sent from Serial1 Tx1
 
-bool circbuf=1; //maintain a circular buffer of midi data received
-bool hexdump=1; //dump midi in data to serial monitor (requires circbuf=1)
+//For debugging:
+bool circbuf=0; //maintain a circular buffer of midi data received
+bool hexdump=0; //dump midi in data to serial monitor (requires circbuf=1)
 
+//Configuration options:
 bool filterAftertouch=1;  //filter aftertouch messages
-bool filterCtrl=0;        //filter control change messages
-bool filterPgm=1;         //filter program change messages
+bool filterCtrl=1;        //filter control change messages
+bool filterPgm=0;         //filter program change messages
 bool filterPressure=1;    //filter channel pressure messages
 bool filterBend=1;        //filter pitch bend messages
 
-bool filterChannel[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+bool filterChannel[] = {0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0}; //filter channel 10 (drums)
+//========================================^ DRUMS
+
+//Enhancement: Set channel filters dynamically using dip switches when no serial data being received.  Smartly debounce the switches.
 
 bool transformAftertouchToVolume = 0;       //transformation for wind controller
 bool transformChannelPressureToVolume = 0;  //transformation for wind controller
@@ -28,8 +33,8 @@ int ixtail=0;         //bytes removed at tail
 int noteDown = LOW; //note on or note off
 int note;
 
-int currchnl;
-int data1;
+int currchnl; //saves current midi channel from status byte
+int data1;    //saves first data byte when midi message has 2 data bytes
 
 void setup() {
     Serial.begin(9600);
@@ -143,7 +148,11 @@ void loop()
           {
             do1ByteMsg(0xC0+currchnl, inbyte);
           }
-          state = 0xC1;  //may have running status          
+          else  //if status byte received: cancel running status and process status byte 
+          {
+            state = 0;
+            doState0();
+          }                    
           break;     
           
         case 0xD1:          
@@ -151,7 +160,11 @@ void loop()
           {
             do1ByteMsg(0xD0+currchnl, inbyte);
           }
-          state = 0xD1;  //may have running status          
+          else  //if status byte received: cancel running status and process status byte 
+          {
+            state = 0;
+            doState0();
+          }                    
           break;     
           
         case 0xE1:
@@ -220,11 +233,13 @@ void doState0 ()
     //stay in state 0 until state changed or a supported status byte received
     if ((inbyte &0xf0) == 0x80)
     {
+       currchnl = inbyte & 0x0F;
        noteDown = LOW;
        state = 0x91;
     }
     else if ((inbyte & 0xf0) == 0x90)
     {
+       currchnl = inbyte & 0x0F;
        noteDown = HIGH;
        state = 0x91;    
     }
@@ -278,27 +293,36 @@ void doNote (byte note, byte velocity, int down) {
   if (down == LOW)
   {
     digitalWrite(LED_BUILTIN, LOW);
-    Serial1.write(0x90);
+    Serial1.write(0x90+currchnl);
     Serial1.write(note);
     Serial1.write(0);
   }
   else
-  {
-    digitalWrite(LED_BUILTIN,HIGH);
-    Serial1.write(0x90);
-    Serial1.write(note);
-    Serial1.write(velocity);
+  {    
+    if (!(filterChannel[currchnl]))
+    {
+      digitalWrite(LED_BUILTIN,HIGH);
+      Serial1.write(0x90+currchnl);
+      Serial1.write(note);
+      Serial1.write(velocity);
+    }
   }
 }
 
 void do2ByteMsg (byte stsbyte, byte dbyte1, int dbyte2) {    
-    Serial1.write(stsbyte);
-    Serial1.write(dbyte1);
-    Serial1.write(dbyte2);
-}
+    if (!(filterChannel[currchnl]))
+    {
+      Serial1.write(stsbyte);
+      Serial1.write(dbyte1);
+      Serial1.write(dbyte2);
+    }
+} 
 
 void do1ByteMsg (byte stsbyte, byte dbyte1) {    
-    Serial1.write(stsbyte);
-    Serial1.write(dbyte1);    
+    if (!(filterChannel[currchnl]))
+    {  
+      Serial1.write(stsbyte);
+      Serial1.write(dbyte1);    
+    }
 }
 
